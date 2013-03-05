038b4b3e29259258fa25b15586b5c49b5eb61741
Moon Limb <ml@euler.local>
2013-03-04 16:23:50 -0800

    cleaning up few folders

--- twitter/mod_stack.py
+++
@@ -1,6 +0,0 @@
-"""
-Design a modified stack that in addition to Push and Pop can also provide minimum element present in the stack via Min fcn
-
-Source: http://www.careercup.com/question?id=12934670
-
-"""
